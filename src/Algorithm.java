bdf3c34336b7a3c78246f0c01ee27db8016e14e5
Jamie W <jamie.won.2000@gmail.com>
2018-02-18 22:55:52 -0500

    Just the start

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,5 +1,13 @@
+/*
+ * Author: Jamie and David
+ * Seating Algorithm (Rough)
+ * Date:
+ * Group - any party wherein all members choose each other (consider overlapping groups for issues)
+ * Priority: 1) any group bigger than maxSeatAtTable/2 2) Time
+ */
+
 
 public class Algorithm {
-	int c = 4;
-ijkml;
+	
+
 }
